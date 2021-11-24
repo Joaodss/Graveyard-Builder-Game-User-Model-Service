822f64b6cd824e3c44f9cde8ec14476996f36ecd
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-24 14:46:01 +0000

    Initialize spring project with dependencies

---
+++ src/main/java/com/ironhack/usermodelservice/UserModelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.usermodelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserModelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserModelServiceApplication.class, args);
+    }
+
+}
